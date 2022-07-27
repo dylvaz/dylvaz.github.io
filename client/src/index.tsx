c48e6d363b803c40736fdd67e52a2603cd253fc8
dylvaz <vazquezadylan@gmail.com>
2022-07-27 11:43:20 -0700

    dark mode

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import theme from './theme';
+
 import './index.css';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
